f51c91debc11b58eeac1425685cffb4cebf4743d
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2022-09-08 22:54:25 +0530

    Create CheckEvenOrOdd.cpp

---
+++ input&output/CheckEvenOrOdd.cpp
@@ -0,0 +1,14 @@
+//C++ Program to Check Whether Number is Even or Odd
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
+    return 0;
+}
