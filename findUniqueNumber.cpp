1d502125c10b3cea8c1ff5c8fe16d9d395d2a412
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-14 22:53:06 +0530

    Update findUniqueNumber.cpp

--- findUniqueNumber.cpp
+++ findUniqueNumber.cpp
@@ -6,3 +6,18 @@ int findUnique(int *arr, int size)
     }
     return ans;
 }
+
+
+
+//without function
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5]={2,3,2,3,4};
+    int ans=0;
+    for(int i=0; i<5; i++){
+        ans=ans^arr[i];
+    }
+    cout<<ans;
+   
+}
