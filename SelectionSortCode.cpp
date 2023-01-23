f5c92a72deecbeab0281eec96131bf9895bc51d6
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-23 20:57:18 +0530

    Update SelectionSortCode.cpp

--- SelectionSortCode.cpp
+++ SelectionSortCode.cpp
@@ -10,3 +10,29 @@ void selectionSort(vector<int>& arr, int n)
          swap(arr[minIndex],arr[i]);
     } 
 }
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+   int arr[n];
+    
+    for(int i=0; i<n; i++){
+        cin>>n;
+    }
+    
+    for(int i=0; i<n-1; i++){
+        int mini=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
+        }
+        swap(arr[i],arr[mini]);
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
