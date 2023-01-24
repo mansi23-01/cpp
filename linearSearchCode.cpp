4a3f291e47b8225677cecd49a70c4fb73f9fb1e1
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-24 09:03:01 +0530

    Update linearSearchCode.cpp

--- linearSearchCode.cpp
+++ linearSearchCode.cpp
@@ -28,3 +28,35 @@ int main() {
 
     return 0;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int key ){
+    for(int i=0; i<size; i++){
+        if(arr[i]==key){
+            return i;
+            
+        }
+    }
+    return -1;
+}
+int main() {
+   int arr[8]={3,4,2,6,5,7,8,1};
+   cout<<"enter the key"<<endl;
+   int key;
+   cin>>key;
+   
+  int found = linearSearch(arr,8,key);
+  
+  if(found==-1){
+      cout<<"key is present"<<endl;
+  }
+  else{
+      cout<<"key is absent"<<endl;
+  }
+
+    return 0;
+}
+
