463599162af05c491badb459abf6b0d8a268f29b
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-24 14:51:03 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -13,3 +13,33 @@ void bubbleSort(vector<int>& arr, int n)
         }
     }
 }
+
+////////////////////////simple
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    bool swapped=false;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[i]){
+               swap(arr[i],arr[j]);
+               swapped=true;
+            }
+        }
+        if(swapped==false){
+            break;
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+    
+    return 0;
+}
