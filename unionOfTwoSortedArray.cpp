c0a4757b746591d489c48f124ce5fef3ed4db155
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-24 23:44:54 +0530

    Create unionOfTwoSortedArray.cpp

---
+++ unionOfTwoSortedArray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    int arr1[n];
+   int arr2[m];
+    for(int i=0; i<n; i++){
+        cin>>arr1[i];
+    }
+     for(int j=0; j<m; j++){
+        cin>>arr2[j];
+    }
+    int i=0,j=0;
+    while(i<n && j<m){
+        if(arr1[i]<arr2[j]){
+            cout<<arr1[i];
+            i++;
+        }
+        else{
+            if(arr1[i]>arr2[j]){
+                cout<<arr2[j];
+                j++;
+            }
+            else{
+                j++;
+                i++;
+            }
+        }
+    }
+    while(i<n){
+        cout<<arr1[i];
+        i++;
+    }
+     while(j<m){
+        cout<<arr2[j];
+        j++;
+    }
+}
