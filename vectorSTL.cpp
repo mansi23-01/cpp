1b302355feaff4655a2fe4368bda283bbb4926f0
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-02-03 13:01:23 +0530

    Create vectorSTL.cpp

---
+++ vectorSTL.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+   vector<int> v;
+   cout<<"capacity is "<<v.capacity()<<endl;
+   
+   v.push_back(1); 
+    cout<<"capacity is "<<v.capacity()<<endl;
+    v.push_back(2); 
+    cout<<"capacity is "<<v.capacity()<<endl;
+    v.push_back(3); 
+    cout<<"capacity is "<<v.capacity()<<endl;
+    cout<<"size is "<<v.size()<<endl;
+    v.push_back(4);
+    cout<<"capacity is "<<v.capacity()<<endl;
+     
+    cout<<"element at second index"<<v.at(2)<<endl;
+    cout<<v.front()<<endl;
+    cout<<v.back()<<endl;
+    
+    cout<<"before pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl;
+    
+    v.pop_back();
+    cout<<"after pop";
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl; 
+    
+    cout<<"before clear"<<v.size()<<endl;
+    
+    v.clear();
+    cout<<"after clear"<<v.size()<<endl;
+    
+    //if we want to give size also:
+    vector <int> a(5,2);
+    cout<<"print vector"<<endl;
+    for(int i:a){
+        cout<<i<<" ";
+    }
+    
+
+    return 0;
+}
