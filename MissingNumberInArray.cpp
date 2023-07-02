7ec578a0262617f9b13830401ef5456399a8b710
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-07-02 23:49:13 +0530

    Update MissingNumberInArray.cpp

--- MissingNumberInArray.cpp
+++ MissingNumberInArray.cpp
@@ -12,3 +12,25 @@ class Solution{
         return sumNaturalNo - sum;
     }
 };
+
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+      cin>>arr[i];
+  }
+  int c=n+1;
+   int natural=(c*(c+1))/2;
+  
+  int sum=0;
+  for(int i=0; i<n; i++){
+      sum+=arr[i];
+  }
+ 
+  cout<<natural-sum;
+
+    return 0;
+}
