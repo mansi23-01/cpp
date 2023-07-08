582c211af5ac28746411d6198623100c9e690590
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-07-08 14:50:07 +0530

    Create Sum-RowWise2DMatrix.cpp

---
+++ Sum-RowWise2DMatrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int arr[3][3];
+   
+   for(int i=0;i<3; i++){
+      for(int j=0; j<3; j++){
+          cin>>arr[i][j];
+      }
+   }
+   
+   for(int i=0; i<3; i++){
+       for(int j=0; j<3; j++){
+           cout<<arr[i][j]<<" ";
+       }
+       cout<<endl;
+   }
+   
+  for(int i=0; i<3; i++){
+      int sum=0;
+       for(int j=0; j<3; j++){
+           sum+=arr[i][j];
+       }
+       cout<<sum;
+       cout<<endl;
+   }
+
+    return 0;
+}
