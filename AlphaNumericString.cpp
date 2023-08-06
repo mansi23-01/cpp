24213bec26b4b267554c6a14ad9d31ae075e428e
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-08-07 00:05:45 +0530

    Update AlphaNumericString.cpp

--- AlphaNumericString.cpp
+++ AlphaNumericString.cpp
@@ -24,3 +24,37 @@ int main() {
 cout<<low<<upp<<num;
 }
 
+
+// using function
+
+ #include <iostream>
+using namespace std;
+void alphaNumeric(string name, int n, string& low, string& upp, string& num){
+ low=upp=num="";
+
+   for(int  i=0; i<n; i++){
+       if(name[i]>='a' && name[i]<='z'){
+           low=low+name[i];
+       }
+       if(name[i]>='A' && name[i]<='Z'){
+           upp=upp+name[i];
+       }
+       if(name[i]>='0' && name[i]<='9'){
+           num=num+name[i];
+       }
+   }
+}
+int main() {
+   string name;
+   cin>>name;
+
+   int n= name.size();
+  
+  string low, upp, num;
+
+ alphaNumeric(name, n, low, upp, num);
+
+  cout<<low<<" "<<upp<<" "<<num;
+
+
+}
