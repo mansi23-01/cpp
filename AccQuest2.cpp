997766247635acbd11fc4a5f53a8123fdd89fc0e
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-08-12 00:51:41 +0530

    Create AccQuest2.cpp

---
+++ AccQuest2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   if(n<=3){
+       cout<<"0";
+       return 0;
+   }
+
+   int arr[n];
+
+   for(int i=0; i<n; i++){
+       cin>>arr[i];
+   }
+
+   vector <int>even;
+   for(int i=0; i<n;i++){
+       if(i%2==0){
+        even.push_back(arr[i]);
+       }
+   }
+  
+   sort(even.begin(), even.end());
+
+   vector <int> odd;
+   for(int i=0; i<n; i++){
+       if(i%2!=0){
+           odd.push_back(arr[i]);
+       }
+   }
+    
+   sort(odd.begin(), odd.end());
+
+   cout<<even[1]+odd[1];
+
+}
+
+//using function
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int addEO(int arr[], int n){
+    if(n<=3){
+       return 0;
+   }
+
+   vector <int>even;
+   for(int i=0; i<n;i++){
+       if(i%2==0){
+        even.push_back(arr[i]);
+       }
+   }
+  
+   sort(even.begin(), even.end());
+
+   vector <int> odd;
+   for(int i=0; i<n; i++){
+       if(i%2!=0){
+           odd.push_back(arr[i]);
+       }
+   }
+    
+   sort(odd.begin(), odd.end());
+
+   return even[1]+odd[1];
+}
+
+int main() {
+     int n;
+   cin>>n;
+
+   int arr[n];
+
+   for(int i=0; i<n; i++){
+       cin>>arr[i];
+   }
+
+   int result= addEO(arr, n);
+   cout<<result;
+}
