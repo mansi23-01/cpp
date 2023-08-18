7772017115738adde4aea88f969405c457797866
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-08-19 01:40:58 +0530

    Update sort0_1.cpp

--- sort0_1.cpp
+++ sort0_1.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 //METHOD 1
-
+#include <iostream>
 #include <vector>
 using namespace std;
 int main() {
@@ -32,3 +31,32 @@ for(int i=0; i<a.size(); i++){
 }
 
 //METHOD 2
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+int n;
+cin>>n;
+int arr[n];
+
+for(int i=0; i<n; i++){
+    cin>>arr[i];
+}
+int count =0;
+for(int i=0; i<n; i++){
+    if(arr[i]==0){
+        count++;
+    }
+}
+for(int i=0; i<count; i++){
+    arr[i]=0;
+}
+for(int i=count; i<n; i++){
+    arr[i]=1;
+}
+
+for(int i=0; i<n; i++){
+    cout<<arr[i];
+}
+}
+
