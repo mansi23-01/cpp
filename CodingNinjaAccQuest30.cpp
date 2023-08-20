a0e219a0b651360329fb118ed96c5b1e7e41db7b
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-08-21 02:57:08 +0530

    Create CodingNinjaAccQuest30.cpp

---
+++ CodingNinjaAccQuest30.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+bool isAnagram(string input1, string input2){
+    for(int i=0; i<input1.size(); i++){
+        bool found= false;
+        for(int j=0; j<input2.size(); j++){
+            if(input1[i]==input2[j]){
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+  string input1, input2;
+  cin>>input1;
+  cin>>input2;
+
+  if(isAnagram(input1, input2)){
+    cout<<"yes";
+  }
+  else{
+      cout<<"no";
+  }
+}
