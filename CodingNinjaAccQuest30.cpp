5bcc3f1164ba5d73fd1f38d9230d1031da2e57d6
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-09-10 21:27:47 +0530

    Update CodingNinjaAccQuest30.cpp

--- CodingNinjaAccQuest30.cpp
+++ CodingNinjaAccQuest30.cpp
@@ -27,3 +27,37 @@ int main() {
       cout<<"no";
   }
 }
+
+//method2
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool isAnagram(string input1, string input2) {
+    if (input1.length() != input2.length()) {
+        return false; // Anagrams must have the same length
+    }
+
+    // Sort both strings
+    sort(input1.begin(), input1.end());
+    sort(input2.begin(), input2.end());
+
+    // Compare the sorted strings
+    return input1 == input2;
+}
+
+int main() {
+    string input1, input2;
+    cin >> input1;
+    cin >> input2;
+
+    if (isAnagram(input1, input2)) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+
+    return 0;
+}
+
